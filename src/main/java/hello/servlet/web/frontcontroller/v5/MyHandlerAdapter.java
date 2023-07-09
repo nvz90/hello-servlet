5a78ec32ace8bf6fd3457d693958d8246d5d5bb0
daeyounggu <ocstar9@gmail.com>
2023-07-09 22:33:33 +0900

    v5 - 핸들어댑터 추가 핸들러의 다형성을 사용하기 위한 인터페이스

--- src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -1,2 +1,15 @@
-package hello.servlet.web.frontcontroller.v5;public interface MyHandlerAdapter {
+package hello.servlet.web.frontcontroller.v5;
+
+import hello.servlet.web.frontcontroller.ModelView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws ServletException, IOException;
 }
